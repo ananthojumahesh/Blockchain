cbd93cde1f229482d3eb1e82b002f8d11dc32594
Remix Workspace <>
2022-10-27 22:41:08 +0530

    added getTaskByIndex, completeTask functions

--- todo.sol
+++ todo.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity 0.8.4;
 
 contract Tasks{
@@ -15,7 +17,15 @@ contract Tasks{
         tasks_list.push(Task(id, task_data, false));
     }
 
-    function getTask() external view returns (Task[] memory){
+    function getTasks() external view returns (Task[] memory){
         return tasks_list;
+    }
+
+    function getTaskByIndex(uint index) external view returns (Task memory){
+        return tasks_list[index];
+    }
+
+    function completeTask(uint id) external {
+        tasks_list[id].isCompleted = true;
     }
 }
